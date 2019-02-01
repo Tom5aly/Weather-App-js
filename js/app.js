4e7e1985ad6ac821c9a4fede99ef6b3fe8efefb7
Tom5aly <tom.aly@hotmail.com>
2019-02-01 19:54:27 +0000

    final

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-class AjaxWheather {
+class AjaxWeather {
   constructor() {
     this.apiKey = "392efce16fe48ca8b41e87c4e4d3e696";
   }
 
-  async wheather(city) {
+  async getWeather(city) {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${
       this.apiKey
-    }`;
+    }&units=metric`;
     const weatherData = await fetch(url);
     const data = await weatherData.json();
     return data;
@@ -17,39 +17,43 @@ class DisplayData {
   constructor() {
     this.results = document.querySelector(".results");
     this.cityName = document.getElementById("cityName");
-    this.cityCountry = document.getElementById("cityCountry");
-    this.cityIcon = document.getElementById("cityIcon");
+    this.country = document.getElementById("country");
     this.cityTemp = document.getElementById("cityTemp");
     this.cityHumidity = document.getElementById("cityHumidity");
+  }
+  showWeather(data) {
+    // console.log(data);
+
+    const {
+      name,
+      sys: { country },
+      main: { temp, humidity }
+    } = data;
+
+    this.results.classList.add("showItem");
+    this.cityName.textContent = name;
+    this.country.textContent = country;
+    this.cityTemp.textContent = temp;
+    this.cityHumidity.textContent = humidity;
   }
 }
 
 (function() {
-  const wheatherForm = document.getElementById("wheatherForm");
+  const weatherForm = document.getElementById("weatherForm");
   const cityInput = document.getElementById("cityInput");
   const feedback = document.querySelector(".feedback");
 
-  const ajax = new AjaxWheather();
+  const ajax = new AjaxWeather();
+  const display = new DisplayData();
 
-  wheatherForm.addEventListener("submit", e => {
+  weatherForm.addEventListener("submit", e => {
     e.preventDefault();
     const city = cityInput.value;
 
     if (city.length === 0) {
-      showFeedback("Please Enter Your City Name");
+      alert("Please Enter Your City Name");
     } else {
-      ajax.wheather(city).then(data => console.log(data));
+      ajax.getWeather(city).then(data => display.showWeather(data));
     }
-    $(document).ready(function() {
-      $(".showItem").hide(12500);
-    });
-  });
-
-  function showFeedback(text) {
-    feedback.classList.add("showItem");
-    feedback.innerHTML = `<p>${text}</p>`;
-  }
-  $(document).ready(function() {
-    $(".showItem").hide(12500);
   });
 })();
